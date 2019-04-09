a541d5443f814307f1e5b1b7fe21aa16b09fe87b
someone <something@apa.com>
2019-04-09 17:09:57 -0400

    FilterSortPaginateTable implemented

--- HorseKata/java/src/main/java/Horse.java
+++ HorseKata/java/src/main/java/Horse.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -12,48 +10,60 @@ public class Horse {
             List<FilterMetadata> filters,
             SortMetadata sortMetadata,
             PaginationMetadata paginationMetadata) {
-        // TODO: filter horse table using filters
-        // TODO: sort horse table using sortMetadata
-        // TODO: paginate horse table using paginationMetadata
-        List<List<String>> dataList = stringifyList(tableData);
-        List<List<String>> filtered =  filterData(headers,dataList,filters);
-        List<List<String>> setOfLists = removeDupicates(filtered);
-        PaginatedTable table = new PaginatedTable(headers,setOfLists,setOfLists.size());
+
+        List<List<Object>> filtered =  filterData(headers,tableData,filters);
+        List<List<String>> dataList = stringifyList(filtered);
+        List<List<String>> sorted = sortList(dataList,headers,sortMetadata);
+        List<List<String>> paginated = paginateList(sorted,paginationMetadata);
+        PaginatedTable table = new PaginatedTable(headers,paginated, paginated.size());
 
         return table;
     }
 
-    private static List<List<String>> filterData(List<String> headers, List<List<String>> data, List<FilterMetadata> filters){
-        List<List<String>> filteredData = new LinkedList<>();
-        for (FilterMetadata fm :filters) {
-           int location =  headers.indexOf(fm.columnHeader);
-           String searched = fm.value.toString();
-           //List<String> strings = data.stream().filter(o ->  )
-
-           for (List l : data) {
-                if(l.get(location).equals(searched)){
-                    filteredData.add(l);
+    private static List<List<Object>> filterData(List<String> headers, List<List<Object>> data, List<FilterMetadata> filters) {
+        if (filters.isEmpty()){
+            return data;
+        }
+        List<List<Object>> filtered = new LinkedList<>();
+        for (List row : data) {
+            boolean matchesAllFilters = true;
+            for (FilterMetadata filter : filters) {
+                int index = headers.indexOf(filter.columnHeader);
+                if (row.get(index) != filter.value) {
+                    matchesAllFilters = false;
                 }
             }
+            if (matchesAllFilters) {
+                filtered.add(row);
+            }
         }
-        return filteredData;
+        return filtered;
     }
 
-    private static List<List<String>> stringifyList(List<List<Object>> objectLists){
+    private static List<List<String>> sortList(List<List<String>> data, List<String> headers, SortMetadata sortMetadata){
+       if(sortMetadata.columnHeader.equals("")){
+           return data;
+       }
+       int index= headers.indexOf(sortMetadata.columnHeader);
+        List<List<String>> sortedList =  data.stream().sorted(Comparator.comparing(row -> row.get(index))).collect(Collectors.toList());
+       if(sortMetadata.sortOrder.equals("Descending")){
+           Collections.reverse(sortedList);
+       }
+       return sortedList;
+    }
+
+    private static List<List<String>> paginateList(List<List<String>> data, PaginationMetadata paginationMetadata){
+        if(paginationMetadata.firstRecordInPage == 0){
+            return data;
+        }
+        return  data.subList(paginationMetadata.firstRecordInPage -1, paginationMetadata.pageSize);
+    }
+
+    private static List<List<String>> stringifyList(List<List<Object>> data){
         List<List<String>> strings;
-        strings = objectLists.stream()
+        strings = data.stream()
                 .map(list -> list.stream().map(o -> o.toString()).collect(Collectors.toList()))
                 .collect(Collectors.toList());
         return strings;
-    }
-    private static List<List<String>> removeDupicates(List<List<String>> lists){
-        List<List<String>> setOfLists = new LinkedList<>();
-        setOfLists.add(lists.get(0));
-        for (List l : lists) {
-            if (!setOfLists.contains(l)){
-                setOfLists.add(l);
-            }
-        }
-        return setOfLists;
     }
 }
--- HorseKata/java/src/test/java/HorseTest.java
+++ HorseKata/java/src/test/java/HorseTest.java
@@ -1,7 +1,10 @@
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -16,37 +19,90 @@ public class HorseTest {
 
     @Before
     public void setup(){
-        headers = SampleHorseData.GetSampleHeaders();
-        tableData = SampleHorseData.GetSampleTableData();
-        filters = SampleHorseData.GetSampleFilters();
-        sortMetadata = SampleHorseData.GetSampleSortMetadata();
-        paginationMetadata = SampleHorseData.GetSamplePaginationMetadata();
+        headers = new ArrayList<>();
+        tableData = new LinkedList<>();
+        filters = new ArrayList<>();
+        sortMetadata = new SortMetadata("","");
+        paginationMetadata = new PaginationMetadata(0,0);
 
-        table = Horse.FilterSortPaginateTable(headers, tableData, filters, sortMetadata, paginationMetadata);
-    }
-
-    @Test
-    public void FilterSortPaginate() {
-        assertEquals(headers, table.headers);
-        assertEquals(2, table.totalRows);
-        assertEquals(Arrays.asList(Arrays.asList("Thoroughbred", "Bay", "1.6", "3", "true"),
-                Arrays.asList("Thoroughbred", "Grey", "1.55", "3", "true")), table.tableData);
     }
 
     @Test
     public void headersAreCorrect(){
+        headers = SampleHorseData.GetSampleHeaders();
+        table = Horse.FilterSortPaginateTable(headers, tableData, filters, sortMetadata, paginationMetadata);
+
         assertEquals(headers, table.headers);
     }
 
     @Test
     public void totalRowsCount(){
-        assertEquals(2, table.totalRows);
+        headers = SampleHorseData.GetSampleHeaders();
+        tableData = SampleHorseData.GetSampleTableData();
+        table = Horse.FilterSortPaginateTable(headers, tableData, filters, sortMetadata, paginationMetadata);
+
+        assertEquals(5, table.totalRows);
     }
 
     @Test
     public void filteredRowsOfData(){
-        assertEquals(Arrays.asList(Arrays.asList("Thoroughbred", "Bay", "1.6", "3", "true"),
-                Arrays.asList("Thoroughbred", "Grey", "1.55", "3", "true")), table.tableData);
+       headers = SampleHorseData.GetSampleHeaders();
+       tableData = SampleHorseData.GetSampleTableData();
+       filters = SampleHorseData.GetSampleFilters();
+       table = Horse.FilterSortPaginateTable(headers, tableData, filters, sortMetadata, paginationMetadata);
 
+       assertEquals(Arrays.asList(Arrays.asList("Thoroughbred", "Bay", "1.6", "3", "true"),
+                Arrays.asList("Thoroughbred", "Grey", "1.55", "3", "true")), table.tableData);
     }
+
+    @Test
+    public void sortRows_byHeightAscending(){
+        headers = SampleHorseData.GetSampleHeaders();
+        tableData = SampleHorseData.GetSampleTableData();
+        sortMetadata = SampleHorseData.GetSampleSortMetadata();
+
+        table = Horse.FilterSortPaginateTable(headers, tableData, filters, sortMetadata, paginationMetadata);
+
+        assertEquals(Arrays.asList(
+                Arrays.asList("Shetland Pony", "Black", "1.01", "2", "false"),
+                Arrays.asList("Arabian horse", "Bay", "1.51", "5", "true"),
+                Arrays.asList("Thoroughbred", "Grey", "1.55", "3", "true"),
+                Arrays.asList("Thoroughbred", "Bay", "1.6", "3", "true"),
+                Arrays.asList("Shire horse", "Black", "1.71", "4", "true")
+                ), table.tableData);
+    }
+
+    @Test
+    public void sortRows_byHeightDescending(){
+        headers = SampleHorseData.GetSampleHeaders();
+        tableData = SampleHorseData.GetSampleTableData();
+        sortMetadata = new SortMetadata("Height", "Descending");;
+
+        table = Horse.FilterSortPaginateTable(headers, tableData, filters, sortMetadata, paginationMetadata);
+
+        assertEquals(Arrays.asList(
+                Arrays.asList("Shire horse", "Black", "1.71", "4", "true"),
+                Arrays.asList("Thoroughbred", "Bay", "1.6", "3", "true"),
+                Arrays.asList("Thoroughbred", "Grey", "1.55", "3", "true"),
+                Arrays.asList("Arabian horse", "Bay", "1.51", "5", "true"),
+                Arrays.asList("Shetland Pony", "Black", "1.01", "2", "false")
+                ), table.tableData);
+    }
+
+    @Test
+    public void paginateData_fromIndex1_pageSize3(){
+        headers = SampleHorseData.GetSampleHeaders();
+        tableData = SampleHorseData.GetSampleTableData();
+        sortMetadata = SampleHorseData.GetSampleSortMetadata();
+        paginationMetadata = SampleHorseData.GetSamplePaginationMetadata();
+
+        table = Horse.FilterSortPaginateTable(headers, tableData, filters, sortMetadata, paginationMetadata);
+
+        assertEquals(Arrays.asList(
+                Arrays.asList("Shetland Pony", "Black", "1.01", "2", "false"),
+                Arrays.asList("Arabian horse", "Bay", "1.51", "5", "true"),
+                Arrays.asList("Thoroughbred", "Grey", "1.55", "3", "true")
+        ), table.tableData);
+    }
+
 }
